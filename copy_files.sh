3dd837d897c6f39c6f0424b9696e051588784388
Eleanor Young <developer@nausicaea.net>
2023-02-21 08:52:45 +0100

    Modified the copy script

--- copy_files.sh
+++ copy_files.sh
@@ -3,7 +3,7 @@
 d=keyboards/crkbd/keymaps/nausicaea_miryoku
 mkdir $d
 
-for f in users/manna-harbour_miryoku/config.h layouts/community/split_3x6_3/manna-harbour_miryoku/config.h
+for f in users/manna-harbour_miryoku/config.h layouts/community/split_3x6_3/manna-harbour_miryoku/config.h keyboards/crkbd/keymaps/default/config.h
 do
   echo "// copied from $f"
   cat $f
@@ -11,7 +11,7 @@ do
   echo
 done > $d/config.h
 
-for f in users/manna-harbour_miryoku/manna-harbour_miryoku.{h,c}
+for f in users/manna-harbour_miryoku/manna-harbour_miryoku.{h,c} keyboards/crkbd/keymaps/default/keymap.c
 do
   echo "// copied from $f"
   cat $f
@@ -19,7 +19,7 @@ do
   echo
 done > $d/keymap.c
 
-for f in users/manna-harbour_miryoku/rules.mk
+for f in users/manna-harbour_miryoku/rules.mk keyboards/crkbd/keymaps/default/rules.mk
 do
   echo "# copied from $f"
   cat $f
